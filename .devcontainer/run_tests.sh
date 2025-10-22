a2b435f2bf9733c04ff7430359edd2f25560fc19
leon todd johnson <126616861+leontoddjohnson@users.noreply.github.com>
2025-10-22 11:21:08 -0400

    Delete .devcontainer/run_tests.sh

--- .devcontainer/run_tests.sh
+++
@@ -1,9 +0,0 @@
-mkdir /temp_grader
-cp -r /autograder/tests /temp_grader/tests
-cp -r . /temp_grader
-cd /temp_grader
-python -m unittest -v tests/test_all.py > /workspaces/week-9/output.txt 2>&1
-cd /workspaces/week-9
-rm -r /temp_grader
-# sed -i '1s/^/Output: \n```python\n/' output.txt
-# echo "\`\`\`" >> output.txt
