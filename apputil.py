538607af9e721e162983b03623bfee99397b8f45
Woods Procise <wprocise@iu.edu>
2025-10-28 18:42:44 -0400

    defining GroupEstimate

--- apputil.py
+++ apputil.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-
-def GroupEstimate(object):
+## Exercise 1
+"""
+Build python class called 'GroupEstimate' taking categorical data and corresponding 
+continuous values, and determines which group a new observation falls into, and 
+predicts an estimate value based on the data provided.
+"""
+# Part 1: Define a class 'GroupEstimate' that accepts an 'estimate' argument, which can be either "mean" or "median".
+class GroupEstimate(object):
     def __init__(self, estimate):
-        self.estimate = estimate
-    
-    def fit(self, X, y):
-        return None
+        if estimate not in ["mean", "median"]:
+            raise ValueError("Estimate must be either 'mean' or 'median'")
 
-    def predict(self, X):
-        return None
+
